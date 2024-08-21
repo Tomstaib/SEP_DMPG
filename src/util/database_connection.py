6285e3fd27ce019908c868451f58b9950b12be42
Marcel <147398052+MarStroe@users.noreply.github.com>
2024-08-21 13:21:20 +0200

    Typo

---
+++ src/util/database_connection.py
@@ -0,0 +1,35 @@
+import os
+import stat
+import sys
+from getpass import getpass
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+import orm
+
+DB_USER = 'sep'
+DB_HOST = 'imt-sep-001.lin.hs-osnabrueck.de'
+DB_PORT = '55432'
+DB_NAME = 'distributed_computing'
+
+def connect_to_db():
+    # Create the database URL
+    db_url = f"postgresql+psycopg2://{DB_USER}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+    # The password will be fetched from .pgpass automatically
+    engine = create_engine(db_url)
+
+    # Test the connection
+    try:
+        connection = engine.connect()
+        print("Connection successful")
+        return connection
+    except Exception as e:
+        print(f"Error: {e}")
+        return None
+
+Session = sessionmaker(bind=connect_to_db())
+session = Session()
+
+model = orm.Model(123,'Test')
+session.add(model)
+session.commit()
+
