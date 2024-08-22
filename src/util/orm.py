6bd51da4dbfa77d44ecd2691e466d33f5e1a93f3
Marcel <147398052+MarStroe@users.noreply.github.com>
2024-08-22 09:00:17 +0200

    Beziehung zwischen User und Model angepasst. Selbes auch auf der Live Datenbank

--- src/util/orm.py
+++ src/util/orm.py
@@ -5,7 +5,6 @@ import psycopg2
 
 Base = declarative_base()
 
-# User Table
 from sqlalchemy import Column, Integer, String, Float, ForeignKey
 from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
@@ -13,24 +12,14 @@ from sqlalchemy.ext.declarative import declarative_base
 Base = declarative_base()
 
 
-# User Table
+# HSUser Table
 class HSUser(Base):
-    __tablename__ = 'HsUser'
+    __tablename__ = 'HSUser'
     user_id = Column(Integer, primary_key=True)
     user_name = Column(String(255))
     number_started_simulations = Column(Integer)
-    model_id = Column(Integer, ForeignKey('Model.model_id'))
 
-    model = relationship('Model', back_populates='hsuser')
-
-    def __init__(self, user_id, user_name, number_started_simulations, model_id):
-        self.user_id = user_id
-        self.user_name = user_name
-        self.number_started_simulations = number_started_simulations
-        self.model_id = model_id
-
-    def __repr__(self):
-        return f"({self.user_id}, {self.user_name}, {self.number_started_simulations})"
+    models = relationship('Model', back_populates='hsuser')
 
 
 # Model Table
@@ -38,16 +27,10 @@ class Model(Base):
     __tablename__ = 'Model'
     model_id = Column(Integer, primary_key=True)
     model_name = Column(String(255))
+    user_id = Column(Integer, ForeignKey('HSUser.user_id'))
 
-    scenarios = relationship('ModelScenario', back_populates='model')
-    hsuser = relationship('HSUser', back_populates='model')
-
-    def __init__(self, model_id, model_name):
-        self.model_id = model_id
-        self.model_name = model_name
-
-    def __repr__(self):
-        return f"({self.model_id}, {self.model_name})"
+    hsuser = relationship('HSUser', back_populates='models')
+    model_scenarios = relationship('ModelScenario', back_populates='model')
 
 
 # Scenario Table
@@ -56,11 +39,9 @@ class Scenario(Base):
     scenario_id = Column(Integer, primary_key=True)
     scenario_name = Column(String(255))
     number_in_system = Column(Integer)
-    avg_time_in_system = Column(Float(20))
-    num_time_in_system = Column(Float(20))
-    total_time_in_system = Column(Float(20))
-    min_time_in_system = Column(Float(20))
-    max_time_in_system = Column(Float(20))
+    avg_time_in_system = Column(Float)
+    max_time_in_system = Column(Float)
+    min_time_in_system = Column(Float)
     number_created = Column(Integer)
     number_destroyed = Column(Integer)
     seed = Column(Integer)
@@ -72,40 +53,15 @@ class Scenario(Base):
     entities = relationship('Entity', back_populates='scenario')
     model_scenarios = relationship('ModelScenario', back_populates='scenario')
 
-    def __init__(self, scenario_id, scenario_name, number_in_system, avg_time_in_system, num_time_in_system,
-                 total_time_in_system,
-                 min_time_in_system, max_time_in_system, number_created, number_destroyed, seed):
-        self.scenario_id = scenario_id
-        self.scenario_name = scenario_name
-        self.number_in_system = number_in_system
-        self.avg_time_in_system = avg_time_in_system
-        self.num_time_in_system = num_time_in_system
-        self.total_time_in_system = total_time_in_system
-        self.min_time_in_system = min_time_in_system
-        self.max_time_in_system = max_time_in_system
-        self.number_created = number_created
-        self.number_destroyed = number_destroyed
-        self.seed = seed
 
-    def __repr__(self):
-        return f"({self.scenario_id}, {self.scenario_name})"
-
-
-# ModelScenario Table (Composite Key)
+# ModelScenario Table
 class ModelScenario(Base):
     __tablename__ = 'Model_Scenario'
     model_id = Column(Integer, ForeignKey('Model.model_id'), primary_key=True)
     scenario_id = Column(Integer, ForeignKey('Scenario.scenario_id'), primary_key=True)
 
-    model = relationship('Model', back_populates='scenarios')
+    model = relationship('Model', back_populates='model_scenarios')
     scenario = relationship('Scenario', back_populates='model_scenarios')
-
-    def __init__(self, model_id, scenario_id):
-        self.model_id = model_id
-        self.scenario_id = scenario_id
-
-    def __repr__(self):
-        return f"({self.model_id}, {self.scenario_id})"
 
 
 # Source Table
@@ -116,21 +72,9 @@ class Source(Base):
     source_name = Column(String(255))
     number_created = Column(Integer)
     number_exited = Column(Integer)
-    creation_time_distribution_with_parameters = Column(Float(20))
+    creation_time_distribution_with_parameters = Column(Float)
 
     scenario = relationship('Scenario', back_populates='sources')
-
-    def __init__(self, source_id, scenario_id, source_name, number_created, number_exited,
-                 creation_time_distribution_with_parameters):
-        self.source_id = source_id
-        self.scenario_id = scenario_id
-        self.source_name = source_name
-        self.number_created = number_created
-        self.number_exited = number_exited
-        self.creation_time_distribution_with_parameters = creation_time_distribution_with_parameters
-
-    def __repr__(self):
-        return f"({self.source_id}, {self.scenario_id}, {self.source_name})"
 
 
 # Server Table
@@ -139,46 +83,22 @@ class Server(Base):
     server_id = Column(Integer, primary_key=True)
     scenario_id = Column(Integer, ForeignKey('Scenario.scenario_id'), primary_key=True)
     server_name = Column(String(255))
-    scheduled_utilization = Column(Float(10))
+    scheduled_utilization = Column(Float)
     units_utilized = Column(Integer)
-    avg_time_processing = Column(Float(10))
-    total_time_processing = Column(Float(10))
+    avg_time_processing = Column(Float)
+    total_time_processing = Column(Float)
     number_entered = Column(Integer)
     number_exited = Column(Integer)
     number_downtimes = Column(Integer)
-    processing_time_distribution_with_parameters = Column(Float(20))
-    time_between_maschine_breakdowns = Column(Float(20))
-    maschine_breakdown_duration = Column(Float(20))
+    total_downtime = Column(Float)
+    processing_time_distribution_with_parameters = Column(Float)
+    time_between_maschine_breakdowns = Column(Float)
+    maschine_breakdown_duration = Column(Float)
     entities_processed = Column(Integer)
-    total_uptime = Column(Float(20))
+    total_uptime = Column(Float)
     number_uptimes = Column(Integer)
 
     scenario = relationship('Scenario', back_populates='servers')
-
-    def __init__(self, server_id, scenario_id, server_name, scheduled_utilization, units_utilized, avg_time_processing,
-                 total_time_processing, number_entered, number_exited, number_downtimes,
-                 processing_time_distribution_with_parameters,
-                 time_between_maschine_breakdowns, maschine_breakdown_duration, entities_processed, total_uptime,
-                 number_uptimes):
-        self.server_id = server_id
-        self.scenario_id = scenario_id
-        self.server_name = server_name
-        self.scheduled_utilization = scheduled_utilization
-        self.units_utilized = units_utilized
-        self.avg_time_processing = avg_time_processing
-        self.total_time_processing = total_time_processing
-        self.number_entered = number_entered
-        self.number_exited = number_exited
-        self.number_downtimes = number_downtimes
-        self.processing_time_distribution_with_parameters = processing_time_distribution_with_parameters
-        self.time_between_maschine_breakdowns = time_between_maschine_breakdowns
-        self.maschine_breakdown_duration = maschine_breakdown_duration
-        self.entities_processed = entities_processed
-        self.total_uptime = total_uptime
-        self.number_uptimes = number_uptimes
-
-    def __repr__(self):
-        return f"({self.server_id}, {self.scenario_id}, {self.server_name})"
 
 
 # Sink Table
@@ -188,26 +108,12 @@ class Sink(Base):
     scenario_id = Column(Integer, ForeignKey('Scenario.scenario_id'), primary_key=True)
     sink_name = Column(String(255))
     entities_processed = Column(Integer)
-    total_time_in_system = Column(Float(20))
+    total_time_in_system = Column(Float)
     number_entered = Column(Integer)
-    max_time_in_system = Column(Float(20))
-    min_time_in_system = Column(Float(20))
+    max_time_in_system = Column(Float)
+    min_time_in_system = Column(Float)
 
     scenario = relationship('Scenario', back_populates='sinks')
-
-    def __init__(self, sink_id, scenario_id, sink_name, entities_processed, total_time_in_system, number_entered,
-                 max_time_in_system, min_time_in_system):
-        self.sink_id = sink_id
-        self.scenario_id = scenario_id
-        self.sink_name = sink_name
-        self.entities_processed = entities_processed
-        self.total_time_in_system = total_time_in_system
-        self.number_entered = number_entered
-        self.max_time_in_system = max_time_in_system
-        self.min_time_in_system = min_time_in_system
-
-    def __repr__(self):
-        return f"({self.sink_id}, {self.scenario_id}, {self.sink_name})"
 
 
 # Connection Table
@@ -218,24 +124,10 @@ class Connection(Base):
     connection_name = Column(String(255))
     entities_processed = Column(Integer)
     number_entered = Column(Integer)
-    processing_duration = Column(Float(20))
-    probability = Column(Float(20))
+    processing_duration = Column(Float)
+    availability = Column(Float)
 
     scenario = relationship('Scenario', back_populates='connections')
-
-    def __init__(self, connection_id, scenario_id, connection_name, entities_processed, number_entered,
-                 processing_duration,
-                 probability):
-        self.connection_id = connection_id
-        self.scenario_id = scenario_id
-        self.connection_name = connection_name
-        self.entities_processed = entities_processed
-        self.number_entered = number_entered
-        self.processing_duration = processing_duration
-        self.probability = probability
-
-    def __repr__(self):
-        return f"({self.connection_id}, {self.scenario_id}, {self.connection_name})"
 
 
 # Entity Table
@@ -244,18 +136,10 @@ class Entity(Base):
     entity_id = Column(Integer, primary_key=True)
     scenario_id = Column(Integer, ForeignKey('Scenario.scenario_id'), primary_key=True)
     entity_name = Column(String(255))
-    creation_time = Column(Float(20))
+    creation_time = Column(Float)
 
     scenario = relationship('Scenario', back_populates='entities')
 
-    def __init__(self, entity_id, scenario_id, entity_name, creation_time):
-        self.entity_id = entity_id
-        self.scenario_id = scenario_id
-        self.entity_name = entity_name
-        self.creation_time = creation_time
-
-    def __repr__(self):
-        return f"({self.entity_id}, {self.scenario_id}, {self.entity_name})"
 
 # Kann genutzt werden um die Tabellen anzulegen
 def create_tables():
