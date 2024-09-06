4e85e0c6e261ef26da5c510d495dad05c8dbf703
Marcel <147398052+MarStroe@users.noreply.github.com>
2024-09-06 15:45:07 +0200

    try-except ergänzt

--- DMPG/src/util/runtime_prediction.py
+++ DMPG/src/util/runtime_prediction.py
@@ -1,29 +1,46 @@
-import json
-import os
 import requests
 import logging
 
-URL = 'https://131.173.65.76:5000/receive_runtime_prediction' #Webserver wird hier angesprochen
+URL = 'https://131.173.65.76:5000/receive_runtime_prediction'  #Webserver wird hier angesprochen
 
 
 def send_progress_to_server(ct, i, step, num_replications):
     data = save_progress(ct, i, step, num_replications)
-    response = requests.post(URL, json=data, verify=False)
 
-    if response.status_code != 200:
-        logging.info(f"Error while sending Runtime-Prediction: {response.status_code}")
-    else:
+    try:
+        response = requests.post(URL, json=data, verify=False)
+        response.raise_for_status()  # Raises an HTTPError for bad responses (4xx and 5xx)
+
         logging.info("Runtime prediction successfully sent to webserver")
+    except requests.exceptions.HTTPError as http_err:
+        logging.error(f"HTTP error occurred while sending Runtime-Prediction: {http_err}")
+    except requests.exceptions.RequestException as req_err:
+        logging.error(f"Request error occurred while sending Runtime-Prediction: {req_err}")
+    except Exception as e:
+        logging.error(f"An unexpected error occurred: {e}")
 
 
 def save_progress(ct, i, step, num_replications):
-    progress_data = {
-        "percentage": ct[0].replace(" ", ""),
-        "time_computed": ct[1].replace("[time computed] ", "").strip(),
-        "time_to_complete": ct[2].replace("[time to complete] ", "").strip(),
-        "time_prediction": ct[3].replace("[time prediction] ", "").strip(),
-        "time_per_iteration": ct[4].replace("[time per iteration] ", "").strip(),
-        "current_iteration": i + 1,
-        "total_iterations": num_replications
-    }
+    try:
+        progress_data = {
+            "percentage": ct[0].replace(" ", ""),
+            "time_computed": ct[1].replace("[time computed] ", "").strip(),
+            "time_to_complete": ct[2].replace("[time to complete] ", "").strip(),
+            "time_prediction": ct[3].replace("[time prediction] ", "").strip(),
+            "time_per_iteration": ct[4].replace("[time per iteration] ", "").strip(),
+            "current_iteration": i + 1,
+            "total_iterations": num_replications
+        }
+    except IndexError as e:
+        # Handle index errors
+        logging.error("Error accessing ct elements: %s", e)
+        return None
+    except AttributeError as e:
+        # Handle attribute errors
+        logging.error("Error processing string attributes: %s", e)
+        return None
+    except Exception as e:
+        # Handle all other exceptions
+        logging.error("Unknown error: %s", e)
+        return None
     return progress_data
