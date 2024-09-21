8f4da6b0cb39d388f360e96a6ac8b0203b405abe
Marcel <147398052+MarStroe@users.noreply.github.com>
2024-09-21 18:53:39 +0200

    Methoden ergänzt: - get_next_simulation_id - get_next_scenario_id - get_next_pivot_table_id - get_next_model_id - commit_session Kommentar in Zeile 131

--- Datenspeicherung/Python/database_connection.py
+++ Datenspeicherung/Python/database_connection.py
@@ -1,14 +1,14 @@
 import logging
 import sys
 import os
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, func
+from sqlalchemy.orm import sessionmaker, Session
 import orm
 from sqlalchemy.exc import OperationalError, SQLAlchemyError
 
 DB_USER = 'sep'
-DB_HOST = 'localhost'
-DB_PORT = '5432'
+DB_HOST = 'imt-sep-001.lin.hs-osnabrueck.de'
+DB_PORT = '55432'
 DB_NAME = 'distributed_computing'
 DB_PASSWORD = 'sep'
 
@@ -20,6 +20,63 @@ def validate_db_config():
     """Validate that all necessary database configuration variables are set."""
     if not all([DB_USER, DB_PASSWORD, DB_HOST, DB_PORT, DB_NAME]):
         raise ValueError("Database configuration is incomplete. Please check all required fields.")
+
+
+def get_next_simulation_id(session: Session) -> int:
+    """Return the next simulation id"""
+    try:
+        highest_id = session.query(func.max(orm.Simulation.simulation_id)).scalar()
+    except Exception as e:
+        print(f"Failed to reach next simulation ID: {e}")
+        return 1
+
+    """If there are no simulation ids yet, return the highest simulation id"""
+    if highest_id is None:
+        print("SimulationsID is None")
+        return 1
+    return highest_id + 1
+
+
+def get_next_scenario_id(session: Session) -> int:
+    """Return the next scenario id"""
+    try:
+        highest_id = session.query(func.max(orm.Scenario.scenario_id)).scalar()
+    except Exception as e:
+        print(f"Failed to reach next szenario ID: {e}")
+        return 1
+
+    """If there are no simulation ids yet, return the highest scenario id"""
+    if highest_id is None:
+        return 1
+    return highest_id + 1
+
+
+def get_next_pivot_table_id(session: Session) -> int:
+    """Return the next pivot table id"""
+    try:
+        highest_id = session.query(func.max(orm.PivotTable.pivot_table_id)).scalar()
+    except Exception as e:
+        print(f"Failed to reach next pivot table ID: {e}")
+        return 1
+
+    """If there are no simulation ids yet, return the highest pivot table id"""
+    if highest_id is None:
+        return 1
+    return highest_id + 1
+
+
+def get_next_model_id(session: Session) -> int:
+    """Return the next model id"""
+    try:
+        highest_id = session.query(func.max(orm.Model.model_id)).scalar()
+    except Exception as e:
+        print(f"Failed to reach next model ID: {e}")
+        return 1
+
+    """If there are no simulation ids yet, return the highest model id"""
+    if highest_id is None:
+        return 1
+    return highest_id + 1
 
 
 def connect_to_db():
@@ -59,8 +116,19 @@ def create_session(engine):
         return None
 
 
+def commit_session(session):
+    try:
+        session.commit()
+        logging.info("Data saved in database")
+    except Exception as e:
+        logging.exception("Session commit failed")
+        session.rollback()
+        logging.exception("Session rollback failed")
+        return None
+
+# Creating the database schema from orm.py
 def main():
-    """Main function to connect to the database and manage sessions."""
+    """Main function to connect to the database and create the database scheme"""
     # Attempt to connect to the database
     engine = connect_to_db()
     if engine:
